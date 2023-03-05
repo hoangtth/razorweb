bd6d97eb3410a1521f0b172079a95ebd85308995
hoangtth <hoangtth.work@gmail.com>
2023-03-05 15:30:34 +0700

    tich hop entity

---
+++ Migrations/20230305074546_initdb.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using Bogus;
+using EntityFrameworkDemo.Models;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EntityFrameworkDemo.Migrations
+{
+    /// <inheritdoc />
+    public partial class initdb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "articles",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: false),
+                    Created = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Content = table.Column<string>(type: "ntext", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_articles", x => x.Id);
+                });
+
+
+
+            //Fake Data : Bogus
+            Randomizer.Seed = new Random(8675309);
+
+            var fakerArticle = new Faker<Article>();
+
+            fakerArticle.RuleFor(a => a.Title, f => f.Lorem.Sentence(5, 5));
+            fakerArticle.RuleFor(a => a.Created, f => f.Date.Between(new DateTime(2021, 1, 1), new DateTime(2021, 7, 30)));
+            fakerArticle.RuleFor(a => a.Content, f => f.Lorem.Paragraphs(1, 4));
+
+            Article article = fakerArticle.Generate();
+
+            // Insert Data
+            for (int i = 0; i < 150; i++)
+            {
+              migrationBuilder.InsertData(
+              table: "articles",
+              columns: new[] { "Title", "Created", "Content" },
+              values: new object[]
+              {
+                    article.Title,
+                    article.Created,
+                    article.Content,
+              }
+              );
+            }
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "articles");
+        }
+    }
+}
